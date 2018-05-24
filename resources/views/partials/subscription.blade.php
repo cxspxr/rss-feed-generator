9fe84c38446427a96cf252f0fd493dc1e221d2f2
casper <ycasperinho@gmail.com>
2018-05-24 14:16:07 +0300

    fix unauth media page

--- resources/views/partials/subscription.blade.php
+++ resources/views/partials/subscription.blade.php
@@ -1,9 +1,11 @@
-@if($user->subscribedTo($media))
-    <a href="{{ route('media.unsubscribe', $media) }}" class="btn btn-sm btn-secondary unsubscribe">
-        Unsubscribe
-    </a>
-@else
-    <a href="{{ route('media.subscribe', $media) }}" class="btn btn-sm btn-info subscribe">
-        Subscribe
-    </a>
-@endif
+@auth
+    @if($user->subscribedTo($media))
+        <a href="{{ route('media.unsubscribe', $media) }}" class="btn btn-sm btn-secondary unsubscribe">
+            Unsubscribe
+        </a>
+    @else
+        <a href="{{ route('media.subscribe', $media) }}" class="btn btn-sm btn-info subscribe">
+            Subscribe
+        </a>
+    @endif
+@endauth
