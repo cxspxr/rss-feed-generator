d26fcdb195b5cad966b344ef6cbbf81f2810fd9c
casper <ycasperinho@gmail.com>
2018-05-24 07:15:40 +0300

    change seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,5 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(MediaTableSeeder::class);
-        $this->call(TagsTableSeeder::class);
     }
 }
